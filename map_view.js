424294f3b829312e6d41d894702bb6b17d32d94a
Isikyus <isikyus@users.noreply.github.com>
2015-11-01 12:09:19 +1100

    Button to remove rooms in text view
    Works, but doesn't look like it due to renders not
    updating properly.
    
    The architecture is still a bit wonky, but it'll do for now.

--- map_view.js
+++ map_view.js
@@ -10,6 +10,8 @@ function($, canvasRenderer, textRenderer) {
     var graphicsContext = canvas.getContext('2d');
     var textContext = $(canvas).parent();
 
+    textRenderer.addListeners(textContext, model);
+
     model.addRoomsListener(function(map) {
       canvasRenderer.render(map, graphicsContext);
       textRenderer.render(map, textContext);
