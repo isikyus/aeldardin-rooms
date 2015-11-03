e32ccaee0c5ef00baa8001e2a475b9a15860904f
Isikyus <edward@palarang.com>
2015-11-03 22:37:16 +1100

    Basic selection model
    Not used for anything yet.

---
+++ selection_model.js
@@ -0,0 +1,46 @@
+define([
+  'jquery'
+],
+function($) {
+  // Simple selection model -- basically a set that supports event listeners.
+
+  var SelectionModel = function() {
+    this.store = {};
+    this.listeners = [];
+  };
+
+  SelectionModel.prototype = {
+    select : function(item) {
+      if (item in this.store) {
+        return false;
+      } else {
+        this.store[item] = true;
+        this.fireRoomsChanged();
+        return true;
+      }
+    },
+    deselect : function(item) {
+      if (item in this.store) {
+        delete this.store[item];
+        this.fireRoomsChanged();
+        return true;
+      } else {
+        return false;
+      }
+    },
+    isSelected : function(item) {
+      return item in this.store;
+    },
+    addListener : function (listener) {
+      this.listeners.push(listener);
+    },
+    fireRoomsChanged : function() {
+      var self = this;
+      $.each(this.listeners, function(_index, listener) {
+        listener(self);
+      });
+    }
+  };
+
+  return SelectionModel;
+});
