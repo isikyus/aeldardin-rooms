1221219b1ddc6f884b87e81886a4ba1e701b1218
Isikyus <edward@palarang.com>
2015-11-22 15:21:19 +1100

    Add the ability to delete the selected rooms

--- map_view.js
+++ map_view.js
@@ -4,13 +4,46 @@ define([
     'text_renderer'
   ],
 function($, canvasRenderer, textRenderer) {
+
+  /*
+   * Generates toolbar buttons (which apply to both the text and HTML view).
+   */
+  var buildToolbar = function() {
+    var toolbarHtml = '<div id="toolbar">' +
+        '<button id="delete_selection">Delete</button>' +
+      '</div>';
+    return $(toolbarHtml);
+  };
+
+  /*
+   * Adds event listeners for the toolbar buttons.
+   *
+   *@param $toolbar The toolbar div, as a jQuery object (hence the $).
+   */
+  var addToolbarListeners = function($toolbar, model) {
+    $toolbar.find('#delete_selection').on('click', function() {
+
+      // TODO: it would probably be more efficient to get the list of selected rooms from the selection model.
+      var toDelete = [];
+      $.each(model.map.getRooms(), function(_index, room) {
+        if (model.selection.isSelected(room.id)) {
+          toDelete.push(room);
+        }
+      });
+      $.each(toDelete, function(_index, room) { model.map.removeRoom(room); });
+    });
+  };
+
   // MVC implementation based on example from <https://alexatnet.com/articles/model-view-controller-mvc-javascript>
 
   var MapView = function(model, canvas) {
     var graphicsContext = canvas.getContext('2d');
+    var toolbar = buildToolbar();
     var textContext = $('<div>');
+    $(canvas).before(toolbar);
     $(canvas).after(textContext);
 
+    addToolbarListeners(toolbar, model);
     textRenderer.addListeners(textContext, model);
 
     var render = function(model) {
