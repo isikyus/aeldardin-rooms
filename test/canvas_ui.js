2b18c8f78724c5e0f1eae4e47c4c112292d09ef7
Isikyus <edward@palarang.com>
2016-04-29 20:00:12 +1000

    Test for adding rooms via canvas
    In preparation for implementation

--- test/canvas_ui.js
+++ test/canvas_ui.js
@@ -44,9 +44,85 @@ function(QUnit, MapController, hitRegions) {
       deleteRooms();
 
       // Confirm that only the third room remains.
+      // TODO: shouldn't depend on an unrelated UI.
       assert.equal(mapDiv.find('#room_0').length, 0, 'Should delete room 0');
       assert.equal(mapDiv.find('#room_1').length, 1, 'Should leave room 1');
       assert.equal(mapDiv.find('#room_2').length, 0, 'Should delete room 2');
+    });
+
+    test('adding rooms', function(assert) {
+      var mapDiv = $('#test-map');
+      var controller = new MapController(mapDiv.find('canvas')[0]);
+
+      // TODO: make this part of common setup code.
+      var regions = hitRegions(mapDiv.find('canvas'));
+
+      // Create a room with exact coordinates.
+      var createExactRoom = function() {
+        regions._fire('dragstart', 1 * scale, 1 * scale);
+        regions._fire('drop', 2 * scale, 4 * scale);
+      };
+
+      // Create a room with inexact coordinates, to test rounding
+      var createInexactRoom = function() {
+        regions._fire('dragstart', 2.1 * scale, 0.9 * scale);
+        regions._fire('drop', 2.7 * scale, 3.4 * scale);
+      };
+
+      createExactRoom();
+      createInexactRoom();
+
+      var rooms = controller.model.map.getRooms();
+      assert.equal(rooms[0].x, 1);
+      assert.equal(rooms[0].y, 1);
+      assert.equal(rooms[0].width, 1);
+      assert.equal(rooms[0].height, 3);
+      assert.equal(rooms[1].x, 2);
+      assert.equal(rooms[1].y, 1);
+      assert.equal(rooms[0].width, 1);
+      assert.equal(rooms[0].height, 2);
+    });
+
+    test('cancelling a room by moving out of the map', function(assert) {
+      var mapDiv = $('#test-map');
+      var controller = new MapController(mapDiv.find('canvas')[0]);
+
+      // TODO: make this part of common setup code.
+      var regions = hitRegions(mapDiv.find('canvas'));
+      var action = controller.model.action;
+
+      // Start creating a room, and check initial state.
+      regions._fire('dragstart', 1 * scale, 1 * scale);
+      assert.equal(action.actionData, { x: 1, y : 1, width: 0, height: 0});
+
+      regions.fire('dragexit', 0, 0);
+      assert.equal(action.actionData, null);
+
+      assert.equal(model.map.getRooms(), []);
+    });
+
+    test('cancelling a room by not making it big enough', function(assert) {
+      var mapDiv = $('#test-map');
+      var controller = new MapController(mapDiv.find('canvas')[0]);
+
+      // TODO: make this part of common setup code.
+      var regions = hitRegions(mapDiv.find('canvas'));
+      var action = controller.model.action;
+
+      // Start creating a room, and check initial state.
+      regions._fire('dragstart', 1 * scale, 1 * scale);
+      assert.equal(action.actionData, { x: 1, y : 1, width: 0, height: 0});
+
+      regions.fire('drag', 4, 3);
+      assert.equal(action.actionData, { x: 1, y : 1, width: 3, height: 2});
+
+      regions.fire('drag', 1, 3);
+      assert.equal(action.actionData, { x: 1, y : 1, width: 0, height: 2});
+
+      regions.fire('drop');
+      assert.equal(action.actionData, null);
+
+      assert.equal(model.map.getRooms(), []);
     });
   };
   return { run : run }
