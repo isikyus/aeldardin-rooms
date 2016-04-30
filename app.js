3a639570c902d89d025390ca98be484f3fc505ae
Isikyus <edward@palarang.com>
2016-04-30 12:06:11 +1000

    Remove the default map from index.html
    No longer necessary now we have the ability to create rooms.
    (Although creating doors is still pretty necessary).

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ function($, MapController) {
   $(function() {
     $('.js-map').each(function(_index, canvas) {
       var controller = new MapController(canvas);
-      //var dataPath = $(canvas).data('src');
-      var mapJson = $('#knossos-map').html();
-      controller.useData(JSON.parse(mapJson));
     });
   });
 });
