872f3cef8699c1679265b1a4573bf99b515e9f33
Isikyus <edward@palarang.com>
2016-04-30 14:40:48 +1000

    Implement addDoor model method

--- test/changes.js
+++ test/changes.js
@@ -8,7 +8,7 @@ define([
 function(QUnit, MapModel) {
   var run = function() {
 
-    QUnit.module('Modifications');
+    QUnit.module('Rooms');
 
     test('adding a room', function(assert) {
       var model = new MapModel();
@@ -38,10 +38,10 @@ function(QUnit, MapModel) {
       });
 
       var result = model.removeRoom(model.getRooms()[1]);
-      assert.deepEqual(result, {id: 1, key: 2, x: 10, y: 7, width: 2, height: 3}, 'Returns removed room on success');
+      assert.deepEqual(result, {id: 1, key: 2, x: 10, y: 7, width: 2, height: 3, wallFeatures: []}, 'Returns removed room on success');
 
       assert.deepEqual(model.getRooms(), [
-        {id: 0, key: 1, x: 10, y: 5, width: 4, height: 2}
+        {id: 0, key: 1, x: 10, y: 5, width: 4, height: 2, wallFeatures: []}
       ]);
     });
 
@@ -55,10 +55,103 @@ function(QUnit, MapModel) {
       });
 
       // Same data, but not the same object.
-      var result = model.removeRoom({key: 1, x: 10, y: 5, width: 4, height: 2});
+      var result = model.removeRoom({key: 1, x: 10, y: 5, width: 4, height: 2, wallFeatures: []});
       assert.notOk(result, 'Returns false on failure');
 
-      assert.deepEqual(model.getRooms(), [{id: 0, key: 1, x: 10, y: 5, width: 4, height: 2}]);
+      assert.deepEqual(model.getRooms(), [{id: 0, key: 1, x: 10, y: 5, width: 4, height: 2, wallFeatures: []}]);
+    });
+
+
+    QUnit.module('Doors');
+
+    test('adding a door', function(assert) {
+      var model = new MapModel();
+      model.setRooms([
+        {id: 0, key: 1, x: 10, y: 5, width: 4, height: 2},
+        {id: 1, key: 2, x: 10, y: 7, width: 2, height: 3}
+      ]);
+
+      assert.expect(3);
+      model.addRoomsListener(function(_rooms) {
+        assert.ok(true, 'Fires change events');
+      });
+
+      var newDoorId = model.addDoor(11, 7, 'north');
+      assert.strictEqual(newDoorId, 0, 'Returns door id on success');
+
+      assert.deepEqual(model.getRooms()[1],
+        {
+          id: 1, key: 2, x: 10, y: 7, width: 2, height: 3,
+          wallFeatures: [
+            {id: newDoorId, x : 11, y: 7, direction: 'north', style: 'door'}
+          ]
+        }
+      );
+    });
+
+    test('adding a door with no containing room', function(assert) {
+      var model = new MapModel();
+      model.setRooms([
+        {id: 0, key: 1, x: 10, y: 5, width: 4, height: 2},
+        {id: 1, key: 2, x: 10, y: 7, width: 2, height: 3}
+      ]);
+
+      assert.expect(2);
+      model.addRoomsListener(function(_rooms) {
+        assert.ok(false, "Doesn't fire events if a change fails.");
+      });
+
+      var result = model.addDoor(5, 7, 'north');
+      assert.strictEqual(result, false, 'Returns false on failure');
+
+      assert.deepEqual(model.getRooms(),[
+        {id: 0, key: 1, x: 10, y: 5, width: 4, height: 2, wallFeatures: []},
+        {id: 1, key: 2, x: 10, y: 7, width: 2, height: 3, wallFeatures: []}
+      ]);
+    });
+
+    test('removing a door', function(assert) {
+      var doorId = 10;
+      var model = new MapModel();
+      model.setRooms([
+        {id: 0, key: 1, x: 10, y: 5, width: 4, height: 2},
+        {
+          id: 1, key: 2, x: 10, y: 7, width: 2, height: 3,
+          wallFeatures: [
+            {id: doorId, x : 11, y: 7, direction: 'north', style: 'door'}
+          ]
+        }
+      ]);
+
+      assert.expect(3);
+      model.addRoomsListener(function(_rooms) {
+        assert.ok(true, 'Fires change events');
+      });
+
+      var result = model.removeDoor(doorId);
+      assert.strictEqual(result, doorId, 'Returns door id on success');
+
+      assert.deepEqual(model.getRooms()[1].wallFeatures, []);
+    });
+
+    test("removing a door that doens't exist", function(assert) {
+      var doorId = 10;
+      var model = new MapModel();
+      model.setRooms([
+        {id: 0, key: 1, x: 10, y: 5, width: 4, height: 2},
+        {id: 1, key: 2, x: 10, y: 7, width: 2, height: 3}
+      ]);
+
+      assert.expect(2);
+      model.addRoomsListener(function(_rooms) {
+        assert.ok(false, "Doesn't fire events if a change fails.");
+      });
+
+      var result = model.removeDoor(doorId);
+      assert.notOk(result, 'Returns false on failure');
+
+      assert.deepEqual(model.getRooms()[0].wallFeatures, []);
+      assert.deepEqual(model.getRooms()[1].wallFeatures, []);
     });
   };
   return { run : run }
